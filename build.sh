98dc12f3b59df4fc35a0e12bca7b4ca43ef186bd
Kevin Scannell <kscanne@gmail.com>
2018-04-02 14:02:27 -0500

    update build scripts and abhar to deal with bilingual site

--- build.sh
+++ build.sh
@@ -4,16 +4,25 @@ ROOTDIR=${HOME}/public_html
 cat abhar.tsv | egrep -v '^#' | sed "s@{GAEILGE}@${GAEILGE}@g; s@{REPOS}@${REPOS}@g" |
 while read x
 do
-	ACTIVE=`echo "${x}" | cut -f 1`
-	SOURCE=`echo "${x}" | cut -f 2`
-	TARGET=`echo "${x}" | cut -f 3`
-	TITLE=`echo "${x}" | cut -f 4`
-	CSS=`echo "${x}" | cut -f 5 | sed 's@\/@\\\/@g'`
+	TEANGA=`echo "${x}" | cut -f 1`
+	ACTIVE=`echo "${x}" | cut -f 2`
+	SOURCE=`echo "${x}" | cut -f 3`
+	TARGET=`echo "${x}" | cut -f 4`
+	TITLE=`echo "${x}" | cut -f 5`
+	CSS=`echo "${x}" | cut -f 6 | sed 's@\/@\\\/@g'`
+	if [ "${TEANGA}" = "en" ]
+	then
+		NAV="nav-en.html"
+		AISTRIUCHAN=`echo "${TARGET}" | sed 's/-en\.html$/.html/'`
+	else
+		NAV="nav.html"
+		AISTRIUCHAN=`echo "${TARGET}" | sed 's/\.html$/-en.html/'`
+	fi
 	if [ ! -e "${SOURCE}" ]
 	then
 		echo "Error: source file ${SOURCE} not found..."
 	else
-		cat nav.html | sed "s/active: false/active: ${ACTIVE}/" | sed "/^<div class=.content.>/r ${SOURCE}" | sed "/^  <title>/s/.*/<title>${TITLE}<\/title>/" > ${ROOTDIR}/${TARGET}
+		cat "${NAV}" | sed "s@AISTRIUCHAN@/${AISTRIUCHAN}@" | sed "s/active: false/active: ${ACTIVE}/" | sed "/^<div class=.content.>/r ${SOURCE}" | sed "/^  <title>/s/.*/<title>${TITLE}<\/title>/" > ${ROOTDIR}/${TARGET}
 		if [ ! "${CSS}" = "-" ]
 		then
 			sed -i "/<link.*cadhan.css/s/.*/&\n  <link rel=\"stylesheet\" href=\"\/${CSS}\">/" ${ROOTDIR}/${TARGET}
@@ -22,3 +31,5 @@ do
 done
 cp cadhan.css ${ROOTDIR}/css
 cp dunaonghusa.png ${ROOTDIR}/pic
+cp favicon.png ${ROOTDIR}/pic
+cp by-sa-80x15.png ${ROOTDIR}/pic
